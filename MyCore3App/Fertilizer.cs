18669fb43c4fbe404ae11b3943e66c3222960ea3
Hugo G Fernandez R <hugh_fernandez@hotmail.com>
2019-01-09 18:21:28 -0600

    Add project files.

---
+++ MyCore3App/Fertilizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MyCore3App
+{
+    public class Fertilizer
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int? MoistureContent { get; set; }
+
+        public int? CNParity { get; set; }
+
+        public float? HumusContent { get; set; }
+
+        public string Notes { get; set; }
+
+        public string Formula { get; set; }
+
+        public string Storage { get; set; }
+
+        public bool IsSoilImprover { get; set; }
+
+        public string Registry { get; set; }
+
+        public int? Pureness { get; set; }
+
+        public bool IsOrganic { get; set; }
+
+        public bool IsInStock { get; set; }
+
+        public int? FertilizerReactionID { get; set; }
+
+        public string ShortAnnualCropApplicationTime { get; set; }
+
+        public string IntermediateAnnualCropApplicationTime { get; set; }
+
+        public string LongAnnualCropApplicationTime { get; set; }
+
+        public string HerbaceousPerennialCropApplicationTime { get; set; }
+
+        public string WoodyPerennialCropApplicationTime { get; set; }
+
+        public int? FertilizerTypeID { get; set; }
+
+        public int? FertilizerAvailabilityID { get; set; }
+
+        public int? FertilizerPresentationID { get; set; }
+
+        public Guid? MediaID { get; set; }
+
+        public Guid ClientID { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public int? UnitID { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ MyCore3App/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace MyCore3App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RestClient client = new RestClient("http://greentegratefertilizationapi.azurewebsites.net/");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetFertilizers();
+        }
+
+        private void GetFertilizers()
+        {
+            var request = new RestRequest("api/clients/38fc8f9e-16d2-40e3-806c-5eb2d2d14e9c/catalogs/Fertilizers", Method.GET);
+            
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            var results = JsonConvert.DeserializeObject<List<Fertilizer>>(content);
+            listFertilizers.ItemsSource = results;            
+        }
+    }
+}
